fc498fb50e080ea08b9011283b2de761b122340e
keshav474 <guptakeshav0828@gmail.com>
2022-01-08 10:42:32 +0530

    Solution of MEANIDIAN question contest START21

---
+++ Mean_equals_Median.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#define FAST     ios_base::sync_with_stdio(0);
+#define INPUT    cin.tie(0);
+#define OUTPUT   cout.tie(0);
+#define ll       long long int
+#define debug(x) cout<<#x<<" = "<<x<<endl;
+#define ms(arr, v) memset(arr, v, sizeof(arr)) //assigns v to all elements of arr
+#define MAXX (ll)(1e18)
+#define lcm(a,b) (a*b)/(__gcd(a,b))
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define endl     "\n"
+#define frr(i, b, n) for (ll i = b; i < n; ++i)
+#define rfrr(i, b, n) for (long i = b; i >= n; --i)
+
+
+using namespace std;
+ 
+unsigned long long int ipow( ll base, ll exp )
+{
+    unsigned long long int result = 1ULL;
+    while( exp )
+    {
+        if ( exp & 1 )
+        {
+            result *= (unsigned long long int)base;
+        }
+        exp >>= 1;
+        base *= base;
+    }
+    return result;
+}
+
+int main()
+{
+    FAST INPUT OUTPUT
+    int t;
+    cin>>t;
+    while(t)
+    {
+        int n;
+        cin>>n;
+        ll arr[n],sum=0,median;
+        frr(i,0,n)
+        {
+            cin>>arr[i];
+            sum+=arr[i];
+            
+        }
+        sort(arr,arr+n);
+        median=arr[(n-1)/2];
+        // debug(sum);
+        // debug(median);
+        
+        if(median*n>=sum)
+        {
+            cout<<median*n-sum;
+        }
+        else
+        {
+            ll res=0,pos=(n-1)/2,midpos=pos;
+            while (sum > n * median)
+            {
+                if (sum + (pos - midpos + 1) * (arr[pos + 1] - arr[pos]) > n * arr[pos + 1])
+                {
+                    sum += (pos - midpos + 1) * (arr[pos + 1] - arr[pos]);
+                    res += (pos - midpos + 1) * (arr[pos + 1] - arr[pos]);
+                    pos++;
+                    median = arr[pos];
+                }
+                else
+                {
+                    ll p = 1 + ((sum - n * arr[pos] - 1) / (n + midpos - pos - 1));
+                    sum += (pos + 1 - midpos) * p;
+                    res += (pos + 1 - midpos) * p;
+                    median = arr[pos] + p;
+                }
+            }
+            cout << res + (n * median - sum);
+
+        }
+        cout<<endl;
+
+ 
+        t--;
+    }
+    return 0;
+}
