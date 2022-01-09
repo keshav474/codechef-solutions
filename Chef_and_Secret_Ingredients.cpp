2fd6e73cbd3dde03916613e37f53b7970fc2249a
keshav474 <guptakeshav0828@gmail.com>
2022-01-09 19:22:26 +0530

    solution to CHEFING problem

---
+++ Chef_and_Secret_Ingredients.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#define FAST     ios_base::sync_with_stdio(0);
+#define INPUT    cin.tie(0);
+#define OUTPUT   cout.tie(0);
+#define ll       long long int
+#define debug(x) cout<<#x<<" = "<<x<<endl;
+#define ms(arr, v) memset(arr, v, sizeof(arr)) //assigns v to all elements of arr
+#define MAXX (ll)(1e18)
+#define lcm(a,b) (a*b)/(__gcd(a,b))
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define endl     "\n"
+#define frr(i, b, n) for (ll i = b; i < n; ++i)
+#define rfrr(i, b, n) for (long i = b; i >= n; --i)
+
+
+using namespace std;
+ 
+unsigned long long int ipow( ll base, ll exp )
+{
+    unsigned long long int result = 1ULL;
+    while( exp )
+    {
+        if ( exp & 1 )
+        {
+            result *= (unsigned long long int)base;
+        }
+        exp >>= 1;
+        base *= base;
+    }
+    return result;
+}
+
+int main()
+{
+    FAST INPUT OUTPUT
+    int t;
+    cin>>t;
+    while(t)
+    {
+        int n;
+        cin>>n;
+        string arr[n];
+        frr(i,0,n)
+        {
+            cin>>arr[i];
+        }
+        int res=0;
+        for(char i='a';i<='z';i++)
+        {
+            int count=0;
+            frr(j,0,n)
+            {
+                                
+                if(arr[j].find(i,0)!=string::npos)
+                count++;
+            }
+            if(count==n)
+            res++;
+        }
+        cout<<res<<endl;
+ 
+        t--;
+    }
+    return 0;
+}
