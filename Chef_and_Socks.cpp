2b5afc1ed052eeba4fc47dfbdc76a368162a0927
keshav474 <guptakeshav0828@gmail.com>
2022-01-14 12:27:08 +0530

    CHEFSOCK cakewalk problem

---
+++ Chef_and_Socks.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define FAST     ios_base::sync_with_stdio(0);
+#define INPUT    cin.tie(0);
+#define OUTPUT   cout.tie(0);
+#define ll       long long int
+#define debug(x) cout<<#x<<" = "<<x<<endl;
+#define ms(arr, v) memset(arr, v, sizeof(arr)) //assigns v to all elements of arr
+#define MAXX (ll)(1e18)
+#define lcm(a,b) (a*b)/(__gcd(a,b))
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define endl     "\n"
+#define frr(i, b, n) for (ll i = b; i < n; ++i)
+#define rfrr(i, b, n) for (long i = b; i >= n; --i)
+
+
+using namespace std;
+ 
+unsigned long long int ipow( ll base, ll exp )
+{
+    unsigned long long int result = 1ULL;
+    while( exp )
+    {
+        if ( exp & 1 )
+        {
+            result *= (unsigned long long int)base;
+        }
+        exp >>= 1;
+        base *= base;
+    }
+    return result;
+}
+
+int main()
+{
+    FAST INPUT OUTPUT
+    int t;
+    t=1;;
+    while(t)
+    {
+        int j,s,m;
+        cin>>j>>s>>m;
+        m=m-j;
+        if((m/s) & 1)
+        cout<<"Unlucky Chef";
+        else
+        cout<<"Lucky Chef";
+        t--;
+    }
+    return 0;
+}
