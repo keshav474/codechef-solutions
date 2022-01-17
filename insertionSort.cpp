8724318fcd48446ec3416eeadbd89bd94ec11bd4
keshav474 <guptakeshav0828@gmail.com>
2022-01-17 23:08:09 +0530

    sorting methods

---
+++ insertionSort.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#define FAST     ios_base::sync_with_stdio(0);
+#define INPUT    cin.tie(0);
+#define OUTPUT   cout.tie(0);
+#define ll       long long int
+#define debug(x) cout<<#x<<" = "<<x<<endl;
+#define ms(arr, v) memset(arr, v, sizeof(arr)) //assigns v to all elements of arr
+#define MAXX (ll)(1e18)
+#define lcm(a,b) (a*b)/(__gcd(a,b))
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define endl     "\n"
+#define frr(i, b, n) for (ll i = b; i < n; ++i)
+#define rfrr(i, b, n) for (long i = b; i >= n; --i)
+
+
+using namespace std;
+ 
+unsigned long long int ipow( ll base, ll exp )
+{
+    unsigned long long int result = 1ULL;
+    while( exp )
+    {
+        if ( exp & 1 )
+        {
+            result *= (unsigned long long int)base;
+        }
+        exp >>= 1;
+        base *= base;
+    }
+    return result;
+}
+
+void insertionSort(int arr[], int n) 
+{ 
+    int i, key, j; 
+    for (i = 1; i < n; i++)
+    { 
+        key = arr[i]; 
+        j = i - 1; 
+
+        while (j >= 0 && arr[j] > key)
+        { 
+            arr[j + 1] = arr[j]; 
+            j = j - 1; 
+        } 
+        arr[j + 1] = key; 
+    } 
+} 
+
+int main()
+{
+    FAST INPUT OUTPUT
+    int t;
+    // cin>>t;
+    t=1;
+    while(t)
+    {
+        int n;
+        cin>>n;
+         int arr[n];
+        frr(i,0,n) cin>>arr[i];
+        insertionSort(arr,n);
+        frr(i,0,n)cout<<arr[i]<<" ";
+        cout<<endl;
+ 
+        t--;
+    }
+    return 0;
+}
---
+++ selectionSort.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#define FAST     ios_base::sync_with_stdio(0);
+#define INPUT    cin.tie(0);
+#define OUTPUT   cout.tie(0);
+#define ll       long long int
+#define debug(x) cout<<#x<<" = "<<x<<endl;
+#define ms(arr, v) memset(arr, v, sizeof(arr)) //assigns v to all elements of arr
+#define MAXX (ll)(1e18)
+#define lcm(a,b) (a*b)/(__gcd(a,b))
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define endl     "\n"
+#define frr(i, b, n) for (ll i = b; i < n; ++i)
+#define rfrr(i, b, n) for (long i = b; i >= n; --i)
+
+
+using namespace std;
+ 
+unsigned long long int ipow( ll base, ll exp )
+{
+    unsigned long long int result = 1ULL;
+    while( exp )
+    {
+        if ( exp & 1 )
+        {
+            result *= (unsigned long long int)base;
+        }
+        exp >>= 1;
+        base *= base;
+    }
+    return result;
+}
+
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_idx; 
+    for (i = 0; i < n-1; i++) 
+    { 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+        if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+        int temp=arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    } 
+} 
+
+
+int main()
+{
+    FAST INPUT OUTPUT
+    int t;
+    // cin>>t;
+    t=1;
+    while(t)
+    {
+        int n;
+        cin>>n;
+         int arr[n];
+        frr(i,0,n) cin>>arr[i];
+        selectionSort(arr,n);
+        frr(i,0,n)cout<<arr[i]<<" ";
+        cout<<endl;
+ 
+        t--;
+    }
+    return 0;
+}
